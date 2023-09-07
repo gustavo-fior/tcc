e0542890b1bc48b74c98e920c2b6c6f6d27e0e15
Gustavo <gustavo.fior@outlook.com>
2023-09-07 19:52:47 -0300

    improve logs

--- code.py
+++ code.py
@@ -18,14 +18,14 @@ base = './base.csv'
 defasado_com_cgpj = './CGP.csv'
 defasado_com_juros = './TXJ.csv'
 
-logar_previsoes = False
+logar_previsoes = True
 media_movel_4_meses = 0
 soma_4_meses = 1
 
 valores = []
 
 with open(defasado_com_juros, 'r') as arquivo_cru:
-    arquivo_lido = csv.reader(arquivo_cru)
+    arquivo_lido = csv.reader(arquivo_cru)  
 
     # Skip the header row
     next(arquivo_lido)
@@ -56,14 +56,16 @@ y_pred_bagging = bagging_regressor.predict(X_test_scaled)
 mse_bagging = mean_squared_error(y_test, y_pred_bagging)
 
 
+
+
 print("--------------- BAGGING -------------\n")
 
 if (logar_previsoes):
-    print(y_pred_bagging)
+    for valor in y_pred_bagging:
+        print(f"{valor}".replace(".", ","))
 
 print("\n")
 print(f'RAIZ QUADRADA DE ERRO DO BAGGING: {mse_bagging}\n')
-print("---------------------------------------\n\n")
 
 # RandomForestRegressor
 random_forest_regressor = RandomForestRegressor(
@@ -75,11 +77,11 @@ mse_rf = mean_squared_error(y_test, y_pred_rf)
 print("-------------- RANDOM FOREST --------------\n")
 
 if (logar_previsoes):
-    print(y_pred_rf)
+    for valor in y_pred_rf:
+        print(f"{valor}".replace(".", ","))
     
 print("\n")
 print(f'RAIZ QUADRADA DE ERRO DO RANDOM FOREST: {mse_rf}\n')
-print("---------------------------------------\n\n")
 
 # XGBoost
 xgb_regressor = XGBRegressor(n_estimators=100, random_state=42)
@@ -90,11 +92,11 @@ mse_xgb = mean_squared_error(y_test, y_pred_xgb)
 print("--------------- XGBOOST ------------\n")
 
 if (logar_previsoes):
-    print(y_pred_xgb)
+    for valor in y_pred_xgb:
+        print(f"{valor}".replace(".", ","))
     
 print("\n")
 print(f'RAIZ QUADRADA DE ERRO DO XGBOOST: {mse_xgb}\n')
-print("---------------------------------------\n\n")
 
 # ARIMA
 arima_model = ARIMA(order=(1, 1, 1))
@@ -105,8 +107,8 @@ mse_arima = mean_squared_error(y_test, y_pred_arima)
 print("--------------- ARIMA (1,1,1) ------------\n")
 
 if (logar_previsoes):
-    print(y_pred_arima)
+    for valor in y_pred_arima:
+        print(f"{valor}".replace(".", ","))
   
 print("\n")
 print(f'RAIZ QUADRADA DE ERRO DO ARIMA: {mse_arima}\n')
-print("---------------------------------------\n\n")
