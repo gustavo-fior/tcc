57364cd284bfc76127bb075eb52f6be6b1eba0ea
Gustavo <gustavo_fior@outlook.com>
2023-09-21 23:31:18 -0300

    fix database and add feature importance analysis

--- code.py
+++ code.py
@@ -9,22 +9,23 @@ from sklearn.ensemble import BaggingRegressor, RandomForestRegressor
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
 from pmdarima.arima import ARIMA
+from sklearn.inspection import permutation_importance
 import warnings
 warnings.filterwarnings("ignore")  # Suppress ARIMA warnings
 
 
 # Load data from CSV
-base = './base.csv'
 defasado_com_cgpj = './CGP.csv'
-defasado_com_juros = './TXJ.csv'
+defasado_com_juros = './txjAA.csv'
 
-logar_previsoes = True
+logar_previsoes = False
+logar_feature_importance = True
 media_movel_4_meses = 0
 soma_4_meses = 1
 
 valores = []
 
-with open(defasado_com_juros, 'r') as arquivo_cru:
+with open(defasado_com_cgpj, 'r') as arquivo_cru:
     arquivo_lido = csv.reader(arquivo_cru)  
 
     # Skip the header row
@@ -37,7 +38,7 @@ valores = np.array(valores)
 
 # Separate features and target variables
 X = valores[:, 2:]
-y = valores[:, soma_4_meses]
+y = valores[:, media_movel_4_meses]
 
 # Split data into learning and test periods
 learning_end_index = 153 + 12 + 6  # Index for June 2020
@@ -49,16 +50,32 @@ scaler = StandardScaler()
 X_learn_scaled = scaler.fit_transform(X_learn)
 X_test_scaled = scaler.transform(X_test)
 
-# BaggingRegressor
+# -----------------------------------------------------
+
+print("--------------- BAGGING -------------\n")
+
 bagging_regressor = BaggingRegressor(n_estimators=100, random_state=42)
 bagging_regressor.fit(X_learn_scaled, y_learn)
 y_pred_bagging = bagging_regressor.predict(X_test_scaled)
 mse_bagging = mean_squared_error(y_test, y_pred_bagging)
 
+base_regressors = bagging_regressor.estimators_
 
+# Initialize a list to store feature importances from all base regressors
+feature_importances = []
 
+# Iterate through each base regressor (decision tree) to extract feature importances
+for base_regressor in base_regressors:
+    if hasattr(base_regressor, 'feature_importances_'):
+        # Some base regressors may not have feature importances, so check if the attribute exists
+        feature_importances.append(base_regressor.feature_importances_)
 
-print("--------------- BAGGING -------------\n")
+# Calculate the average feature importances across all base regressors
+average_feature_importances = np.mean(feature_importances, axis=0)
+
+if (logar_feature_importance):
+    for valor in average_feature_importances:
+        print(f"{valor}")
 
 if (logar_previsoes):
     for valor in y_pred_bagging:
@@ -67,14 +84,19 @@ if (logar_previsoes):
 print("\n")
 print(f'RAIZ QUADRADA DE ERRO DO BAGGING: {mse_bagging}\n')
 
-# RandomForestRegressor
+# -----------------------------------------------------------
+
+print("-------------- RANDOM FOREST --------------\n")
+
 random_forest_regressor = RandomForestRegressor(
     n_estimators=100, random_state=42)
 random_forest_regressor.fit(X_learn_scaled, y_learn)
 y_pred_rf = random_forest_regressor.predict(X_test_scaled)
 mse_rf = mean_squared_error(y_test, y_pred_rf)
 
-print("-------------- RANDOM FOREST --------------\n")
+if (logar_feature_importance):
+    for valor in random_forest_regressor.feature_importances_:
+        print(f"{valor}")
 
 if (logar_previsoes):
     for valor in y_pred_rf:
@@ -83,13 +105,19 @@ if (logar_previsoes):
 print("\n")
 print(f'RAIZ QUADRADA DE ERRO DO RANDOM FOREST: {mse_rf}\n')
 
-# XGBoost
+# ----------------------------------------------------------
+
+print("--------------- XGBOOST ------------\n")
+
 xgb_regressor = XGBRegressor(n_estimators=100, random_state=42)
 xgb_regressor.fit(X_learn_scaled, y_learn)
 y_pred_xgb = xgb_regressor.predict(X_test_scaled)
 mse_xgb = mean_squared_error(y_test, y_pred_xgb)
 
-print("--------------- XGBOOST ------------\n")
+if (logar_feature_importance):
+    for valor in xgb_regressor.feature_importances_:
+        print(f"{valor:.20f}")
+
 
 if (logar_previsoes):
     for valor in y_pred_xgb:
@@ -98,13 +126,15 @@ if (logar_previsoes):
 print("\n")
 print(f'RAIZ QUADRADA DE ERRO DO XGBOOST: {mse_xgb}\n')
 
-# ARIMA
+# ----------------------------------------------------------
+
+print("--------------- ARIMA (1,1,1) ------------\n")
+
 arima_model = ARIMA(order=(1, 1, 1))
 arima_model_fit = arima_model.fit(y_learn)
 y_pred_arima = arima_model_fit.predict(n_periods=len(y_test))
 mse_arima = mean_squared_error(y_test, y_pred_arima)
 
-print("--------------- ARIMA (1,1,1) ------------\n")
 
 if (logar_previsoes):
     for valor in y_pred_arima:
